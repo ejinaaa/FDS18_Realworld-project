845822601f8858cb5ede5573b8853cbe6fa49126
HaJunRyu <hjr4557@naver.com>
2021-02-22 10:16:20 +0900

    create devlop branch and initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/index.ts'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './public/index.html'),
+      inject: 'body',
+      filename: path.join(__dirname, './dist/index.html')
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  },
+  devtool: 'source-map',
+  mode: 'development'
+};
