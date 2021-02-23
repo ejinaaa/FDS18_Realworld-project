b53f383f74822939b21b8fffc68f61c0434ebc0e
HaJunRyu <hjr4557@naver.com>
2021-02-23 17:25:35 +0900

    feat: implements article create page solved: #13

--- src/components/edit.ts
+++ src/components/edit.ts
@@ -20,18 +20,18 @@ class Edit extends View {
           <form>
             <fieldset>
               <fieldset class="form-group">
-                  <input type="text" class="form-control form-control-lg" placeholder="Article Title">
+                  <input type="text" class="article-title form-control form-control-lg" placeholder="Article Title">
               </fieldset>
               <fieldset class="form-group">
-                  <input type="text" class="form-control" placeholder="What's this article about?">
+                  <input type="text" class="article-description form-control" placeholder="What's this article about?">
               </fieldset>
               <fieldset class="form-group">
-                  <textarea class="form-control" rows="8" placeholder="Write your article (in markdown)"></textarea>
+                  <textarea class="article-body form-control" rows="8" placeholder="Write your article (in markdown)"></textarea>
               </fieldset>
               <fieldset class="form-group">
-                  <input type="text" class="form-control" placeholder="Enter tags"><div class="tag-list"></div>
+                  <input type="text" class="article-tag-list form-control" placeholder="Enter tags"><div class="tag-list"></div>
               </fieldset>
-              <button class="btn btn-lg pull-xs-right btn-primary" type="button">
+              <button class="btn btn-lg btn-publish pull-xs-right btn-primary" type="button">
                   Publish Article
               </button>
             </fieldset>
@@ -44,7 +44,30 @@ class Edit extends View {
   }
 
   eventBinding(): void {
-    
+    const $articleTitle = document.querySelector('.article-title') as HTMLInputElement;
+    const $articleDescription = document.querySelector('.article-description') as HTMLInputElement;
+    const $articleBody = document.querySelector('.article-body') as HTMLTextAreaElement;
+    const $articleTagList = document.querySelector('.article-tag-list') as HTMLInputElement;
+    const $btnPublish = document.querySelector('.btn-publish') as HTMLButtonElement;
+
+    $btnPublish.addEventListener('click', () => {
+      const title = $articleTitle.value;
+      const description = $articleDescription.value;
+      const body = $articleBody.value;
+      const tagList = $articleTagList.value;
+
+      axios.post('https://conduit.productionready.io/api/articles', {
+        article: {
+        title,
+        description,
+        body,
+        tagList
+        }
+      },
+      { headers: { Authorization: `Token ${this.USER_TOKEN}` } });
+
+      navigateTo('/');
+    });
   }
 }
 
