19df13f00f1a90fc8549de622f9252f66d83ce5c
HaJunRyu <hjr4557@naver.com>
2021-02-24 13:47:29 +0900

    fix: not go to same route solved: #19

--- src/utils/navigateTo.ts
+++ src/utils/navigateTo.ts
@@ -1,6 +1,7 @@
 import routerHandler from './routerHandler';
 
 const navigateTo = (url: string) => {
+  if(window.location.href === url) return;
   window.history.pushState(null, '', url);
   routerHandler();
 };
