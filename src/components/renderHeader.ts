26763c7326c5106039d85ddbfddb82dcd0375c5c
ejinaaa <eunjink0423@gmail.com>
2021-02-24 10:15:06 +0900

    feat: Profile section in progress and update header section

--- src/components/renderHeader.ts
+++ src/components/renderHeader.ts
@@ -1,17 +1,13 @@
-import axios from "axios";
+import getData from "./getData";
 
 const renderHeader = async () => {
   const userToken = window.localStorage.getItem('JWT');
-  const nowPage = window.location.pathname;
+  const currentPage = window.location.pathname;
   let userName = '';
   
   if (userToken) {
-      const userInfoData = await axios('https://conduit.productionready.io/api/user', {
-        headers: {
-          Authorization: `Token ${userToken}`
-        }
-      });
-      userName = userInfoData.data.user.username;
+      const userInfo = await getData('user'); 
+      userName = userInfo.data.user.username;
   }
 
   return `<nav class="navbar navbar-light">
@@ -20,23 +16,23 @@ const renderHeader = async () => {
     <ul class="nav navbar-nav pull-xs-right">
       <li class="nav-item">
         <!-- Add "active" class when you're on that page" -->
-        <a class="nav-link ${nowPage === '/' ? 'active' : ''}" href="/">Home</a>
+        <a class="nav-link ${currentPage === '/' ? 'active' : ''}" href="/">Home</a>
       </li>
       <li class="nav-item">
-      ${userToken ? `<a class="nav-link ${nowPage === '/editor' ? 'active' : ''}" href="/editor">
+      ${userToken ? `<a class="nav-link ${currentPage === '/editor' ? 'active' : ''}" href="/editor">
       <i class="ion-compose"></i>&nbsp;New Post
     </a>` : ''}
       </li>
       <li class="nav-item">
         <a class="nav-link ${
-          nowPage === '/login' ? 'active' : nowPage === '/settings' ? 'active' : ''
+          currentPage === '/login' ? 'active' : currentPage === '/settings' ? 'active' : ''
         }" href="${userToken ? '/settings' : '/login'}">
-          ${userToken ? 'Settings' : 'Sign in'}
+          ${userToken ? '<i class="ion-gear-a"></i>&nbsp;Settings' : 'Sign in'}
         </a>
       </li>
       <li class="nav-item">
         <a class="nav-link ${
-          nowPage === '/register' ? 'active' : nowPage === '/profile' ? 'active' : ''
+          currentPage === '/register' ? 'active' : currentPage === '/profile' ? 'active' : ''
         }" href="${userToken ? '/profile' : '/register'}">${userToken ? userName : 'Sign up'}</a>
       </li>
     </ul>
