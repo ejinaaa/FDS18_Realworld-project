4b2c8a329dea9756829c210e17c62a6431b5583d
HaJunRyu <hjr4557@naver.com>
2021-02-24 14:49:56 +0900

    feat: add webpack.config into style-loader, css-loader, sass-loader solved: #20

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/
       }
     ]
   },
