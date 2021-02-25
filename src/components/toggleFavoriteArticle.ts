1e6523b174d4271b2e96133c7a717670511e1027
ejinaaa <eunjink0423@gmail.com>
2021-02-25 17:18:15 +0900

    feat: Implement toggle favorite button of article section cloased: #34

---
+++ src/components/toggleFavoriteArticle.ts
@@ -0,0 +1,24 @@
+import request from '../api/request';
+import Articles from '../interface/Articles';
+
+const toggleFavoriteArticle = async (e: MouseEvent) => {
+  const targetElement = e.target as HTMLElement;
+  const targetParentElement = targetElement.closest('.article-meta') as HTMLDivElement;
+  const targetParentSiblingElement = targetParentElement?.nextElementSibling as HTMLAnchorElement;
+  const targetBtn = targetElement.closest('.btn') as HTMLButtonElement;
+  const slug = targetParentSiblingElement.href.split('@')[1];
+  const selectedArticleFavorited: boolean = (await request.getArticle(slug)).data.article.favorited;
+  let articleInfo: Articles;
+
+  if (!selectedArticleFavorited) {
+    articleInfo = (await request.favoriteArticle(slug)).data.article;
+  } else {
+    articleInfo = (await request.unfavoriteArticle(slug)).data.article;
+  }
+
+  const favoritesCount = articleInfo.favoritesCount;
+
+  targetBtn.innerHTML = `<i class="ion-heart"></i> ${favoritesCount}`;
+};
+
+export default toggleFavoriteArticle;
